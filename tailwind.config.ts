8796818059fca3b04dbab00a4aa5796daddb17af
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-05-04 04:22:07 +0200

    🎨 feat(styles): add Tailwind CSS configuration and enhance global styles

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "*.{js,ts,jsx,tsx,mdx}",
+  ],
+  prefix: "",
+  plugins: [],
+} satisfies Config; 
